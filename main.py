ab5ce61ad8f37bd48738bac5e312268866014649
dgsaltarin <dgsaltarin@gmail.com>
2021-10-19 07:09:18 -0500

    fixed bug

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import math
 
 OPTIONS = [
     '1. Calcular valor futuro',
-    '2. Calcular número de periodos',
-    '3. Calcular interes',
+    '2. Calcular interes',
+    '3. Calcular número de periodos',
     '4. Calcular valor presente',
     '5. Calcular valor futuro con tasa nominal',
     '6. Convertir tasa nominal mensual a tasa efectiva anual',
@@ -16,15 +16,15 @@ def calculate_future_value(present_value, interest, periods):
 
 
 def calculate_interest(present_value, future_value, periods):
-    return (future_value / present_value) ** (1 / periods) - 1
+    return ((future_value / present_value)**(1 / periods)) - 1
 
 
 def calculate_periods(present_value, future_value, interest):
-    return math.log(future_value / present_value) / math.log(1 + interest)
+    return math.log(future_value / present_value) / math.log(1 + (interest/100))
 
 
 def calculate_present_value(future_value, interest, periods):
-    return future_value / (1 + interest) ** periods
+    return future_value / (1 + (interest/100))**periods
 
 
 def calculate_effective_interest_rate(nominal_rate):
@@ -47,42 +47,42 @@ def run():
     selected_option = str(input('Escoja la opción que desea: '))
     while selected_option != '7':
         if selected_option == '1':
-            present_value = int(input(present_value_label))
-            interest = int(input(interest_label))
-            periods = int(input(periods_label))
+            present_value = float(input(present_value_label))
+            interest = float(input(interest_label))
+            periods = float(input(periods_label))
             future_value = calculate_future_value(present_value, interest, periods)
             print(f'El valor futuro de su inversión es de: {future_value}')
 
         if selected_option == '2':
-            present_value = int(input(present_value_label))
-            future_value = int(input(future_value_label))
-            periods = int(input(periods_label))
+            present_value = float(input(present_value_label))
+            future_value = float(input(future_value_label))
+            periods = float(input(periods_label))
             interest = calculate_interest(present_value, future_value, periods)
             print(f'El interes de su inversión es de: {interest}')
 
         if selected_option == '3':
-            present_value = int(input(present_value_label))
-            interest = int(input(interest_label))
-            future_value = int(input(future_value_label))
+            present_value = float(input(present_value_label))
+            interest = float(input(interest_label))
+            future_value = float(input(future_value_label))
             periods = calculate_periods(present_value, future_value, interest)
             print(f'El número de periodos de su inversión es de: {periods}')
 
         if selected_option == '4':
-            future_value = int(input(future_value_label))
-            interest = int(input(interest_label))
-            periods = int(input(periods_label))
+            future_value = float(input(future_value_label))
+            interest = float(input(interest_label))
+            periods = float(input(periods_label))
             present_value = calculate_present_value(future_value, interest, periods)
             print(f'El valor presente de su inversión es de: {present_value}')
 
         if selected_option == '5':
-            present_value = int(input(present_value_label))
-            interest = int(input(interest_label))
-            periods = int(input(periods_label))
+            present_value = float(input(present_value_label))
+            interest = float(input(interest_label))
+            periods = float(input(periods_label))
             future_value = calculate_future_value_with_nominal_rate(present_value, interest, periods)
             print(f'El futuro de su inversión con interes nominal es de: {future_value}')
 
         if selected_option == '6':
-            nominal_rate = int(input('Ingrese el porcentaje de su tasa nominal mensual: '))
+            nominal_rate = float(input('Ingrese el porcentaje de su tasa nominal mensual: '))
             effective_interest_rate = calculate_effective_interest_rate(nominal_rate)
             print(f'El interes nominal mensual convertido a efectivo anual es del: {effective_interest_rate}%')
 
